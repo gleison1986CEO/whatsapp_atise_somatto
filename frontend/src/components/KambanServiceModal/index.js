3807602c3ab30a8c3735b617b093589023bb360a
ReynaldoAnalista <33528889+ReynaldoAnalista@users.noreply.github.com>
2025-01-24 15:33:46 -0300

    mantendo só o hinova

--- frontend/src/components/KambanServiceModal/index.js
+++ frontend/src/components/KambanServiceModal/index.js
@@ -113,11 +113,9 @@ const KambamServiceModal = ({ open, onClose, scheduleId, contactId, cleanContact
 		if (open) {
 			try {
 				(async () => {
-					const { data: contactList } = await api.get('/contacts/list', { params: { companyId: companyId } });
 					const { data: contactListHinova } = await api.post('/hinova_users');
-					let customList = contactList.map((c) => ({ id: c.id, name: c.name }));
 					let customListHinova = contactListHinova.map((c) => ({ id: c.codigo_associado, name: c.nome }));
-					let combinedList = [{ id: "", name: "" }, ...customList, ...customListHinova];
+					let combinedList = [{ id: "", name: "" }, ...customListHinova];
 					combinedList.sort((a, b) => a.name.localeCompare(b.name));
 					if (isArray(combinedList)) {
 						setContacts(combinedList);
